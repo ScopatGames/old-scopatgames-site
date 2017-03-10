984edc1333e4ca79867afe29ec3bd63124f1d112
Scott Patten <scopatten@gmail.com>
2017-03-09 19:53:59 -0500

    ...

--- app/js/pages/Contact.js
+++ app/js/pages/Contact.js
@@ -9,7 +9,7 @@ export default class Contact extends React.Component {
           <p><span className="contact-prefix">Email: </span><a href="mailto:scopatten@gmail.com">scopatten@gmail.com</a></p>
           <p><span className="contact-prefix">GitHub: </span><a href="https://github.com/ScopatGames">https://github.com/ScopatGames</a></p>
           <p><span className="contact-prefix">LinkedIn: </span><a href="https://www.linkedin.com/in/scott-patten-916880106">https://www.linkedin.com/in/scott-patten-916880106</a></p>
-          <p><span className="contact-prefix">Resume: </span><a href="http://scottpatten.net/Patten_Scott_Resume.pdf">Download</a></p>
+          <p><span className="contact-prefix">Resume: </span><a href="Patten_Scott_Resume.pdf">Download</a></p>
         </div>
       </section>
     )
