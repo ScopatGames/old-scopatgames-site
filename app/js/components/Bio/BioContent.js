b6f9b4da9d2e797eebdac5bf05e4e1430f05b73f
Scott Patten <scopatten@gmail.com>
2017-04-04 18:30:36 -0400

    changed color scheme

--- app/js/components/Bio/BioContent.js
+++ app/js/components/Bio/BioContent.js
@@ -32,19 +32,19 @@ export default class BioContent extends React.Component {
             <img src={imgBuoy} width="100%"/>
             <Bird/>
           </div>
-          <svg className="svg-water" viewBox="0 0 2000 1200" width="2000" preserveAspectRatio="xMidYMin slice">
+          <svg className="svg-water" viewBox="0 0 2000 800" width="2000" preserveAspectRatio="xMidYMin slice">
             <defs>
               <linearGradient id="water-gradient" x1="0%" y1="0%" x2="0%" y2="100%">
-                  <stop offset="0%" style={{stopColor:"rgb(14, 218, 255)", stopOpacity: 0.07}} />
-                  <stop offset="35%" style={{stopColor:"rgb(0, 47, 93)", stopOpacity:0.6}} />
+                  <stop offset="0%" style={{stopColor:"rgb(71, 86, 101)", stopOpacity: 0.98}} />
+                  <stop offset="35%" style={{stopColor:"rgb(0, 47, 93)", stopOpacity:0.99}} />
                   <stop offset="85%" style={{stopColor:"rgb(0,8,16)", stopOpacity:1}} />
               </linearGradient>
             </defs>
           </svg>
           <div className="introduction-content" id="bio">
               <h2>My skills</h2>
-              
-              
+
+
               <ul className="intro-ticked-list">
                   <li>Web Layout and Design (HTML, CSS, D3)</li>
                   <li>Programming (JavaScript, C#, Java)</li>
@@ -58,13 +58,13 @@ export default class BioContent extends React.Component {
                   <li>3D Modeling and Animation (Blender, Rhino, AutoCAD)</li>
                   <li>Game Development (Unity)</li>
               </ul>
-              
+
               <div className="pic-sig-wrapper">
                 <div className="profile-pic">
-                  
+
                 </div>
                 <div className="intro-signature">
-                    
+
                       <a className="btn-code" href="#portfolio">All fun and games...</a>
                 </div>
               </div>
--- app/js/components/Bio/BioFixedBackground.js
+++ app/js/components/Bio/BioFixedBackground.js
@@ -14,21 +14,21 @@ export default class BioFixedBackground extends React.Component {
       starColorDark: "rgb(80, 73, 4)",
       starRadius: "1.5"
     };
-    starsBackground.initialize(starsInputs);
-    starsBackground.startTwinkle();
+    //starsBackground.initialize(starsInputs);
+    //starsBackground.startTwinkle();
   }
 
   render(){
     return (
       <div className="fixed-background">
-        <svg className="svg-background" height="1080px" width="100%">
+        {/*<svg className="svg-background" height="1080px" width="100%">
             <rect style= {{ fill: "url(#polyline-1)" }} x="0" y="0" height="100%" width="100%"></rect>
             <defs>
                 <pattern id="polyline-1" patternUnits="userSpaceOnUse" width="100" height="200">
                     <polyline points="50,33.33335 50,166.66665 100,200 0,0 50,33.33335 100,0 0,200 0,133.33335 100,66.66665 100,0 0,0 0,66.66665 100,133.33335 100,200 0,200 50,166.66665" style={{fill:"none",stroke:"#211f1a",strokeWidth:"1"}}/>
                 </pattern>
             </defs>
-        </svg>
+        </svg>*/}
       </div>
     );
   }
