264e79570c51679ccb517d806b416d68938b17df
Scott Patten <scopatten@gmail.com>
2017-04-05 17:21:01 -0400

    tweaked alignment

--- app/js/pages/Contact.js
+++ app/js/pages/Contact.js
@@ -6,10 +6,10 @@ export default class Contact extends React.Component {
       <section className="section-contact" id="contact">
         <div className="contact-container">
           <h1>Make Contact</h1>
-          <p><span className="contact-prefix">Email: </span><a href="mailto:scopatten@gmail.com">scopatten@gmail.com</a></p>
-          <p><span className="contact-prefix">GitHub: </span><a href="https://github.com/ScopatGames">https://github.com/ScopatGames</a></p>
-          <p><span className="contact-prefix">LinkedIn: </span><a href="https://www.linkedin.com/in/scott-patten-916880106">https://www.linkedin.com/in/scott-patten-916880106</a></p>
-          <p><span className="contact-prefix">Resume: </span><a href="Patten_Scott_Resume.pdf">Download</a></p>
+          <p><span className="contact-prefix"></span><a href="mailto:scopatten@gmail.com">scopatten@gmail.com</a></p>
+          <p><span className="contact-prefix"></span><a href="https://github.com/ScopatGames">GitHub</a></p>
+          <p><span className="contact-prefix"></span><a href="https://www.linkedin.com/in/scott-patten-916880106">LinkedIn</a></p>
+          <p><span className="contact-prefix"></span><a href="Patten_Scott_Resume.pdf">Download Resume (PDF)</a></p>
         </div>
       </section>
     )
